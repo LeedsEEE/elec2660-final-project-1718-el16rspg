38f864b53da59c62dcd5d79419bf5ee9111b2250
Rohan Garg <rohangarg@campus-10-41-93-180.wireless.leeds.ac.uk>
2017-12-02 13:50:57 +0000

    Initial Commit

---
+++ ResistanceIsFutile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ResistanceIsFutile
+//
+//  Created by Rohan Garg on 02/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ResistanceIsFutile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ResistanceIsFutile
+//
+//  Created by Rohan Garg on 02/12/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
